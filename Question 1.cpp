d490902049cdcc0956a26860b708665ec765721b
Tani-99 <127846818+Tani-99@users.noreply.github.com>
2023-03-23 21:56:11 +0530

    Update Question 1.cpp

---
+++ Question 1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    
+    int number;
+    cin>>number;
+    
+    if(number%2==0){
+        cout<<"The number is even.";
+    }
+    else{
+        cout<<"The number is odd.";
+    }
+    return 0;
+}
